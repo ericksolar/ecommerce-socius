1658a9c2f9aa7e00a4c512db8f1b230eaae0adad
Erick Solar <ericksolarb@gmail.com>
2024-09-06 18:47:13 -0400

    Guardando cambios locales antes de hacer pull

--- ecommerce/obj/Debug/net6.0/ecommerce.AssemblyInfo.cs
+++ ecommerce/obj/Debug/net6.0/ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d880f53308dae4e4730cfe3932028e9a6fff59d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
