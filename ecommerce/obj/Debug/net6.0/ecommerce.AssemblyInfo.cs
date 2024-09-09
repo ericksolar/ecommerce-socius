2fad9750a263d98442000f3553b6c178eb0a72f7
Erick Solar <ericksolarb@gmail.com>
2024-09-08 21:45:18 -0300

    Eliminar appsettings.json del historial

--- ecommerce/obj/Debug/net6.0/ecommerce.AssemblyInfo.cs
+++ ecommerce/obj/Debug/net6.0/ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764f1b88f2e96ad2b7579bcefb9e4bfd84229a5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
